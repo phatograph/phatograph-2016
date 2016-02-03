6adb6a73eac30f029c19b931514597062b9bbcb8
Phat Wangrungarun <phatograph@gmail.com>
2016-02-03 13:03:33 +0000

    Renaming

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ var app         = express();
 var static_path = path.join(__dirname, 'build');
 var port        = 4000;
 
-app.use(express.static(static_path))
-  .get('/*', function (req, res) {
-    res.sendFile('index.html', {
-      root: static_path
-    });
-  }).listen(process.env.PORT || port, function (err) {
-    if (err) { console.log(err) };
-    console.log(`Listening at localhost:${port}`);
+app.use(express.static(static_path));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {
+    root: static_path
   });
+});
+
+app.listen(process.env.PORT || port, function (err) {
+  if (err) { console.log(err) };
+  console.log(`Listening at localhost:${port}`);
+});
